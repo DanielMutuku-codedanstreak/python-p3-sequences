589b2983f20a06647a374922d6a770050bc82a55
Daniel Mutuku <danielmakau507@gmail.com>
2023-11-24 12:01:29 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci_sequence = []
+    if length < 1:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+    else:
+        fibonacci_sequence = [0,1]
+        while len(fibonacci_sequence) < length :
+            fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    print(fibonacci_sequence)
